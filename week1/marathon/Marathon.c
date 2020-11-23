fb51da2241efbc037c0ae6d72608e13f92860dd7
Soumyanil Biswas <61424547+Soumyanil-Biswas@users.noreply.github>
2020-11-23 06:54:29 +0530

    Files Updated

--- week1/marathon/Marathon.c
+++ week1/marathon/Marathon.c
@@ -3,7 +3,7 @@
 
 int main(void) // int ----> program within returns integer
 {
-    int miles = 26, yards = 385;
+    int miles = 26, yards = 385; 
     double kilometers;
 
     kilometers = 1.609 * (miles + yards / 1760.0 ); // n yard = n/1760 mile
--- week1/prgm1/prgm1.c
+++ week1/prgm1/prgm1.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 	printf("Hello World\n");
 	return 0;
 }
 
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	printf('Hello World\n'); ------> This is wrong.    --------------------------->   In C, '' != ""  <----------------------
+	return 0;
+}
+
+*/

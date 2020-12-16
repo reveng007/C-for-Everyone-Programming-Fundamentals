bf614659b1ac76cc8566fb40c93d2b8876c8ba4e
Soumyanil Biswas <61424547+Soumyanil-Biswas@users.noreply.github.com>
2020-12-16 12:49:56 +0000

    Files Updated

--- week5/pointer/12.Pointer_arithmetic-Increment_and_Decrement.c
+++ week5/pointer/12.Pointer_arithmetic-Increment_and_Decrement.c
@@ -16,21 +16,21 @@ pre/post increaments:
 
 #include<stdio.h>
 
-int post_incre(int a[], int *p)
+int post_incre(int a[])
 {
-    printf("%d\n", *(p++)); 
-    // p++ => s1: p = 1000 ==> *p = *(1000) = 5  | s2: p = p + (1 x 4) [as in this case it is array] = 1000 + 4 = 1004 ==> *p = *(1004) = 16
+    printf("%d\n", *(a++)); 
+    // p++ => s1: a = 1000 ==> *a = *(1000) = 5  | s2: a = a + (1 x 4) [as in this case it is array] = 1000 + 4 = 1004 ==> *p = *(1004) = 16
     // so, *(p++) = 5
-    printf("%d\n", *p); // In next step, *p = 16
+    printf("%d\n", *a); // In next step, *a = 16
 }
 
 
-int pre_incre(int a[], int *p)
+int pre_incre(int a[])
 {
-    printf("%d\n", *(++p)); 
-    // ++p => s1: p = p + (1 x 4) [as in this case it is array] = 1004 ==> *p = *(1004) = 16  | s2: p = 1004 ==> *p = *(1004) = 16
-    // so, *(++p) = 16
-    printf("%d\n", *p); // In next step, *p = 16
+    printf("%d\n", *(++a)); 
+    // ++p => s1: a = a + (1 x 4) [as in this case it is array] = 1004 ==> *a = *(1004) = 16  | s2: a = 1004 ==> *a = *(1004) = 16
+    // so, *(++a) = 16
+    printf("%d\n", *a); // In next step, *a = 16
 }
 
 int post_decre(int a[])
@@ -53,16 +53,18 @@ int pre_decre(int a[])
     // so, *(--p) = 16
     printf("%d\n", *p); // In next step, *p = 16
 }
+
 int main()
 {
     int a[] = {5, 16, 7, 89, 45, 32, 23, 10};
-    int *p = &a[0]; // Suppose base address of the array is 1000
+    
+    // Suppose base address of the array is 1000
     
     printf("Post Increment:\n");
-    post_incre(a,p);
+    post_incre(a);
 
     printf("Pre Increment:\n");
-    pre_incre(a,p);
+    pre_incre(a);
 
     printf("Post Decrement:\n");
     post_decre(a);
